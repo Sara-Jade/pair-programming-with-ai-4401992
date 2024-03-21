e518d2f853d161af9aaab2f827aa1da3d93fc5c8
Sara-Jade <88349392+Sara-Jade@users.noreply.github.com>
2024-03-21 21:18:20 +0000

    Documented img component

--- example-vite/src/components/Image.jsx
+++ example-vite/src/components/Image.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * dynImg() is a helper function that returns a URL to the image file
+ * @param {*} imgData 
+ * @param {*} folder 
+ * @param {*} type 
+ * @returns a URL object
+ */
 const dynImg = (imgData, folder, type) => {
   let requestURI;
+
+  // If a folder is specified, use it
   if (folder) {
     requestURI = `${folder}/${imgData.root}.${type}`;
   } else {
     requestURI = `${imgData.root}.${type}`;
   }
+
+  // Return a URL object
   return new URL(`../../public/images/${requestURI}`, import.meta.url);
 };
 
+/**
+ * Image is a component that renders an image
+ * @param {*} a props object containing imgData, folder, and type
+ * @returns an <img/> element
+ */
 export const Image = ({ imgData, folder, type }) => {
   return (
     <img
