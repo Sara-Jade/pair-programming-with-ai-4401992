c178b32498e2fcdcc499ea157cd43c71765f1085
Sara-Jade <88349392+Sara-Jade@users.noreply.github.com>
2024-03-22 22:28:54 +0000

    Fixed errors using Bing copilot

--- example-errors/script.js
+++ example-errors/script.js
@@ -1,15 +1,14 @@
 function openDialogPlus(dialogId, focusAfterClosed, focusFirst) {
-  setTimeout(function () 
-  {
+  setTimeout(function () {
     if (document.body.scrollHeight > window.innerHeight) {
       document.body.style['padding-right'] = window.innerWidth - document.documentElement.clientWidth + 'px';
     }
     document.body.style['overflow'] = 'hidden';
     openDialog(dialogId, focusAfterClosed, focusFirst)
   }, mobileBreakpointMq.matches ? 50 : 10);
-};
+}
 
-function dialogBackdropTransitionendHandler() {
+function dialogBackdropTransitionendHandler(event) {
   if (event.target !== this || event.propertyName !== 'opacity' || getComputedStyle(this)['opacity'] !== '0') {
     return;
   }
@@ -18,7 +17,7 @@ function dialogBackdropTransitionendHandler() {
   this.querySelector('[role="dialog"]').style['display'] = 'none';
 };
 
-function textButtonKeydownHandler() {
+function textButtonKeydownHandler(event) {
   if (event.key === ' ') {
     event.preventDefault();
   }
@@ -27,8 +26,8 @@ function textButtonKeydownHandler() {
 
 function medStr(n) {
   const nArr = n.split(',').map(Number).sort((a, b) => a - b);
-  const e = nArr.length % 2 = 0;
-  return e ? (nArr[nArr.length / 2 - 1] + nArr[nArr.length * 2]) / 2 : nArr[(nArr.length - 1) / 2];
+  const isEven = nArr.length % 2 === 0;
+  return isEven ? (nArr[nArr.length / 2 - 1] + nArr[nArr.length / 2]) / 2 : nArr[(nArr.length - 1) / 2];
 }
-const median = medStr(42,87,9,63,18,76,55,29,91,5,37,68,12);
+const median = medStr('42, 87, 9, 63, 18, 76, 55, 29, 91, 5, 37, 68, 12');
 console.log(median); 
